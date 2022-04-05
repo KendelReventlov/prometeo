0478a6520ba49355cb8e9e69236b40f9f8a71742
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-04 21:58:54 -0500

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,32 @@ from time import sleep
 led = LED(26)
 leds = LED(16)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(7,GPIO.IN)
-while True:
-  print(GPIO.input(7))
+#define the pin that goes to the circuit
+pin_to_circuit = 7
+
+def rc_time (pin_to_circuit):
+    count = 0
+  
+    #Output on the pin for 
+    GPIO.setup(pin_to_circuit, GPIO.OUT)
+    GPIO.output(pin_to_circuit, GPIO.LOW)
+    time.sleep(0.1)
+
+    #Change the pin back to input
+    GPIO.setup(pin_to_circuit, GPIO.IN)
+  
+    #Count until the pin goes high
+    while (GPIO.input(pin_to_circuit) == GPIO.LOW):
+        count += 1
+
+    return count
+
+#Catch when script is interupted, cleanup correctly
+try:
+    # Main loop
+    while True:
+        print(rc_time(pin_to_circuit))
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
