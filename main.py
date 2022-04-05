e6510e259df4cdb596656fd5a9f749733753fd5d
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-04 22:09:21 -0500

    main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ import time
 
 while True:
   sensor = Adafruit_DHT.DHT11 #Cambia por DHT22 y si usas dicho sensor
-  pin = 4 #Pin en la raspberry donde conectamos el sensor
+  pin = 7 #Pin en la raspberry donde conectamos el sensor
   humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
 
   print ('Humedad: ' , humedad)
