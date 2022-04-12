62132bb1c1f0397ccdf7597995ad3952a18e9228
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-12 09:48:11 -0500

    Update main.rs

--- ampere/src/main.rs
+++ ampere/src/main.rs
@@ -1,13 +1,5 @@
-extern crate websocket;
-extern crate image;
-extern crate image_base64;
-use websocket::ClientBuilder;
+
 
 fn main(){
-  loop{
-    let base = image_base64::to_base64("imagen.jpg");
-    let mut cliente = ClientBuilder::new("ws://192.168.100.10:3000/ws").unwrap().connect_insecure().unwrap();
-    cliente.send_message(&websocket::Message::text(base)).unwrap();
-    std::thread::sleep_ms(5000);
-  }
+  println!("Hola raspberry!");
 }
