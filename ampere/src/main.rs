6a03da8a0a826bbbc9d4b19d43f0a9868adba8b7
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-12 10:47:42 -0500

    Update main.rs

--- ampere/src/main.rs
+++ ampere/src/main.rs
@@ -1,21 +1,10 @@
 extern crate websocket;
 extern crate image_base64;
-extern crate rscam;
 use websocket::ClientBuilder;
 
 fn main(){
-  let mut camara = rscam::new("dev/video0").unwrap();
-  camara.start(&rscam::Config{
-    interval:(1,30),
-    resolution:(720,480),
-    format:b"MJPG",
-    ..Default::default()
-  });
 
   loop{
-    let frame = camara.capture().unwrap();
-    let mut archivo = std::fs::File::create("imagen.png").unwrap();
-    archivo.write_all(&frame[..]).unwrap();
     let base = image_base64::to_base64("imagen.png");
     let mut cliente = ClientBuilder::new("ws://192.168.100.10:3000/ws").unwrap().connect_insecure().unwrap();
     cliente.send_message(&websocket::Message::text(base)).unwrap();
