79dbc3bd799e2dc1578e81599ceedd229c4e30bd
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-12 10:13:12 -0500

    Update main.rs

--- ampere/src/main.rs
+++ ampere/src/main.rs
@@ -5,7 +5,7 @@ use websocket::ClientBuilder;
 
 fn main(){
   loop{
-    let base = image_base64::to_base64("imagen.jpg");
+    let base = image_base64::to_base64("imagen.png");
     let mut cliente = ClientBuilder::new("ws://192.168.100.10:3000/ws").unwrap().connect_insecure().unwrap();
     cliente.send_message(&websocket::Message::text(base)).unwrap();
     std::thread::sleep_ms(5000);
