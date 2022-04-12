4aadaec10ad268740a418aa6b0f543b903339759
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-12 11:12:10 -0500

    Update camera.py

--- ampere/camera.py
+++ ampere/camera.py
@@ -1,9 +1,13 @@
 from picamera import PiCamera
+import RPI.GPIO as gpio
+
+gpio.setmode(gpio.BOARD)
+gpio.setup(26,gpio.OUT)
+gpio.output(26,True)
 
 camera = PiCamera()
 camera.resolution = (2592,1944)
 camera.framerate = 15
 camera.annotate_text="Hola champis"
-camera.brightness = 60
 camera.capture("./imagen.png")
 print("HECHO")
