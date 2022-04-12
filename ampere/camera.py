575d0ff6fd984f64aac3ccca7014fe2d123834c4
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-12 11:04:27 -0500

    Update camera.py

--- ampere/camera.py
+++ ampere/camera.py
@@ -4,6 +4,6 @@ camera = PiCamera()
 camera.resolution = (2592,1944)
 camera.framerate = 15
 camera.annotate_text="Hola champis"
-camara.brightness = 100
+camara.brightness = 70
 camera.capture("./imagen.png")
 print("HECHO")
