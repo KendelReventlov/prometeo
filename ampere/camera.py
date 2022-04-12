8acfaf8272e1183ab1c09f92c0b0e92e9874fa37
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-12 10:58:11 -0500

    Update camera.py

--- ampere/camera.py
+++ ampere/camera.py
@@ -1,7 +1,7 @@
 from picamera import PiCamera
 
 camera = PiCamera()
-camera.resolution(2592,1944)
+camera.resolution = (2592,1944)
 camera.framerate = 15
 camera.annotate_text="Hola champis"
 camera.capture("./imagen.png")
