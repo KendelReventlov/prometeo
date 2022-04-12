a46d686d19620d8b570cc225f6fbf56431c8b547
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-12 11:31:23 -0500

    Update main.rs

--- ampere/src/main.rs
+++ ampere/src/main.rs
@@ -6,11 +6,12 @@ use std::process::Command;
 fn main(){
 
   let output = Command::new("python").arg("camera.py");
+  std::thread::sleep_ms(5000);
 
   loop{
     let base = image_base64::to_base64("imagen.png");
     let mut cliente = ClientBuilder::new("ws://192.168.100.10:3000/ws").unwrap().connect_insecure().unwrap();
     cliente.send_message(&websocket::Message::text(base)).unwrap();
-    std::thread::sleep_ms(5000);
+    std::thread::sleep_ms(2500);
   }
 }
