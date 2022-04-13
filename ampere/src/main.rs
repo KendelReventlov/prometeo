de016a0773be2d2b06ff21a5a72c01c9b16ffebc
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-12 20:37:28 -0500

    Update main.rs

--- ampere/src/main.rs
+++ ampere/src/main.rs
@@ -17,7 +17,7 @@ fn main(){
 
   loop{
     let cuadro = camera.capture().unwrap();
-    println!("{}",cuadro);
+    println!("{:?}",cuadro);
     let mut cliente = ClientBuilder::new("ws://192.168.100.10:3000/ws").unwrap().connect_insecure().unwrap();
     cliente.send_message(&websocket::Message::text("hola")).unwrap();
     std::thread::sleep_ms(5000);
