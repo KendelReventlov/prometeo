9ed021e2cd1f5b2ec0f07e824a563191380eaeac
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-12 20:36:06 -0500

    Update main.rs

--- ampere/src/main.rs
+++ ampere/src/main.rs
@@ -1,17 +1,25 @@
 extern crate websocket;
 extern crate image_base64;
+extern crate rscam;
 use websocket::ClientBuilder;
 use std::process::Command;
 
 fn main(){
 
-  let output = Command::new("python").arg("camera.py");
-  std::thread::sleep_ms(5000);
+  let mut camera = rscam::new("/dev/video0").unwrap();
+
+  camera.start(&rscam::Config{
+    interval: (1,30),
+    resolution: (1080,720),
+    format: b"MJPG",
+    ..Default::default()
+  }).unwrap();
 
   loop{
-    let base = image_base64::to_base64("imagen.png");
+    let cuadro = camera.capture().unwrap();
+    println!("{}",cuadro);
     let mut cliente = ClientBuilder::new("ws://192.168.100.10:3000/ws").unwrap().connect_insecure().unwrap();
-    cliente.send_message(&websocket::Message::text(base)).unwrap();
-    std::thread::sleep_ms(2500);
+    cliente.send_message(&websocket::Message::text("hola")).unwrap();
+    std::thread::sleep_ms(5000);
   }
 }
