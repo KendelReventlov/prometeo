b9f6bbf78fa4f6c4bf48e2e79e8d4776bb9cd665
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-14 08:13:17 -0500

    Update main.rs

--- ampere/src/main.rs
+++ ampere/src/main.rs
@@ -1,29 +1,26 @@
 extern crate websocket;
 extern crate image_base64;
-extern crate rascam;
+extern crate rpi_video_rs;
 
 use websocket::ClientBuilder;
 use std::process::Command;
-use rascam::*;
-use std::{thread,time};
+use rpi_video_rs::recorder::Recorder;
 
-fn main(){
+fn main(){ 
 
-  let info = info().unwrap();
-  if info.cameras.len() < 1 {
-    println!("No se encontraron camaras!")
-    std::process::exit(0);
-  }
-
-  let mut camara = SimpleCamera::new(info.cameras[0]).unwrap();
-  camara.activate().unwrap();
-  thread::sleep(time::Duration::from_secs(2));  
+  let mut recorder = Recorder::new(None);
 
   loop{
     let mut cliente = ClientBuilder::new("ws://192.168.100.10:3000/ws").unwrap().connect_insecure().unwrap();
     cliente.send_message(&websocket::Message::text("hola")).unwrap();
-    let b = camara.take_one().unwrap();
-    println!("BUFFER: {:?}",b);
-    println!();
+    match recorder.run(){
+      Ok(res)=>{
+        println!("Video guardado en el path: ",res.output_file_path);
+      }
+      Err(_)=>{
+        println!("Ocurrió un error!");
+      }
+    }
+    
   }
 }
