86b0853b294d45563495915f3a095f34d32ec393
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-14 18:31:00 -0500

    Update main.rs

--- ampere/src/main.rs
+++ ampere/src/main.rs
@@ -1,11 +1,11 @@
 extern crate websocket;
 extern crate image_base64;
-
+extern crate mmal_sys;
 use websocket::ClientBuilder;
 
 fn main(){
-  loop{
+  /*loop{
     let mut cliente = ClientBuilder::new("ws://192.168.100.10:3000/ws").unwrap().connect_insecure().unwrap();
     cliente.send_message(&websocket::Message::text("hola")).unwrap();
-  }
+  }*/
 }
