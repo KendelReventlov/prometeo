6a727c5e1d38d480534d2ebbcecc909048595cc5
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-14 18:09:54 -0500

    Update camera.py

--- ampere/camera.py
+++ ampere/camera.py
@@ -5,4 +5,14 @@ import cv2
 led = LED(26)
 led.on()
 
+video_objeto = cv2.VideoCapture("./video.mp4")
+count = 0
+exito = 1
+
+while exito:
+  exito,imagen = video_objeto.read()
+  cv2.imwrite("frame%d.jpg" % count,image)
+  count+=1
+  print(imagen)
+
 print("HECHO")
