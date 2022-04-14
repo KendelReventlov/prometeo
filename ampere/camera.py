fbf4fb2cd53b8122fcd35762c0a5d3d0c317e00d
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-14 18:03:17 -0500

    Update camera.py

--- ampere/camera.py
+++ ampere/camera.py
@@ -1,16 +1,8 @@
-from picamera import PiCamera
 from gpiozero import LED
 from time import sleep
+import cv2
 
 led = LED(26)
 led.on()
-
-camera = PiCamera()
-camera.resolution = (2592,1944)
-camera.framerate = 15
-sleep(2)
-while True:
-  camera.annotate_text="Hola champis"
-  camera.capture("./imagen.png")
 
 print("HECHO")
