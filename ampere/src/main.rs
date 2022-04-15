466dec662f9cb688e3f412b018f85e7dee7fc4ec
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-14 19:41:13 -0500

    Update main.rs

--- ampere/src/main.rs
+++ ampere/src/main.rs
@@ -5,23 +5,24 @@ use std::process::Command;
 use std::net::TcpStream;
 use std::io::prelude::*;
 
-mod executer;
-
 fn main(){
 
-  executer::iniciar_tcp_camara();
-  /*Command::new("raspivid").args(["-t","0"]).arg("-l").args([
-    "-o",
-    "tcp://127.0.0.1:7878",
-  ]);
-  std::thread::sleep_ms(10000);
-  */let mut stream = TcpStream::connect("127.0.0.1:7878").unwrap();
+  std::thread::spawn(move ||{
+    let comando = std::process::Command::new("raspivid").args([
+        "-t","0"
+    ]).arg("-l").args([
+        "-o",
+        "tcp://127.0.0.1:7878",
+    ]).spawn().unwrap();
+    println!("COMANDO: {:?}",comando);
+
+});
+
+  let mut stream = TcpStream::connect("127.0.0.1:7878").unwrap();
   let mut buffer = [0;128];
-  loop{
-    stream.read(&mut buffer).unwrap();
-    println!("BUFFER: {:?}",buffer);
-    println!();
-  }
+  stream.read(&mut buffer).unwrap();
+
+  println!("BUFFER: {:?}",buffer);
   /*loop{
     let mut cliente = ClientBuilder::new("ws://192.168.100.10:3000/ws").unwrap().connect_insecure().unwrap();
     cliente.send_message(&websocket::Message::text("hola")).unwrap();
