5e5c96080f087eaef352d56067e16d4ff9f9fe2c
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-15 09:44:26 -0500

    Update main.rs

--- ampere/src/main.rs
+++ ampere/src/main.rs
@@ -2,36 +2,35 @@ extern crate websocket;
 extern crate base64;
 use websocket::ClientBuilder;
 use std::process::Command;
+use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
 
 extern crate png;
 use png::*;
 
+fn handler(stream: TcpStream){
+  let mut buffer = Vec::new();
+  stream.read_to_end(&mut buffer).unwrap();
+  println!("VEC: {:?}",buffer);
+}
+
 fn main(){
-  loop{
-    let comando = std::process::Command::new("raspistill").args([
-      "-t",
-      "150",
-    ]).args([
-      "-w",
-      "720",
-    ]).args([
-      "-h",
-      "480",
-    ]).args([
-      "-o",
-      "imagen.jpg",
-    ]).status().unwrap();
 
-    println!("SALIDA: {:?}",comando);
-
-    let mut archivo = Vec::new();
-    std::fs::File::open(std::path::Path::new("imagen.jpg")).unwrap().read_to_end(&mut archivo).unwrap();
-    println!("LEN FILE: {:?}",archivo.len());
+  let servidor = TcpListener::bind("127.0.0.1:3000").unwrap();
+  let comando = std::process::Command::new("raspistill").args([
+    "t",
+    "0",
+  ]).args([
+    "-o",
+    "tcp://127.0.0.1:3000",
+  ]);
+  for stream in servidor.incoming(){
+    handler(stream.unwrap());
+  }
+  /*loop{
 
     let mut cliente = ClientBuilder::new("ws://192.168.100.10:3000/ws").unwrap().connect_insecure().unwrap();
-    cliente.send_message(&websocket::Message::text(base64::encode(&archivo))).unwrap();
-    println!("Se envió exitosamente el mensaje!");
-  }
+    cliente.send_message(&websocket::Message::text(base64::encode(&buffer))).unwrap();
+  }*/
 }
