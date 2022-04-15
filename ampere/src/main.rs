c491e9eef94160f1ffc3f834b35d7b2a2784b3bd
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-14 20:45:29 -0500

    Update main.rs

--- ampere/src/main.rs
+++ ampere/src/main.rs
@@ -34,7 +34,7 @@ fn main(){
 
   
   loop{
-    let mut buffer = [0;614400];
+    let mut buffer = [0;5000];
     stream.read(&mut buffer).unwrap();
     println!("BUFFER: {:?}",buffer);
     println!("LEN BUFFER: {}",buffer.len());
