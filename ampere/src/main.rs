72e62f5b9bab129feeb50494bd2ec7ad87278889
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-14 19:22:00 -0500

    Update main.rs

--- ampere/src/main.rs
+++ ampere/src/main.rs
@@ -6,9 +6,11 @@ use std::net::TcpStream;
 use std::io::prelude::*;
 
 fn main(){
-  let comando = Command::new("raspivid").output().unwrap();
+  let comando = Command::new("raspivid").args(["-t","0"]).arg("-l").args([
+    "-o",
+    "tcp://127.0.0.1:7878",
+  ]).status();
   println!("SALIDA: {:?}",comando);
-  std::thread::sleep_ms(5000);
   let mut stream = TcpStream::connect("127.0.0.1:7878").unwrap();
   stream.write(&[1]).unwrap();
   let mut buffer = [0;128];
