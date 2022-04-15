6927e61bd7bfa7649754e1f4a72ac8f8b4e97731
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-14 19:43:39 -0500

    Update main.rs

--- ampere/src/main.rs
+++ ampere/src/main.rs
@@ -6,23 +6,19 @@ use std::net::TcpStream;
 use std::io::prelude::*;
 
 fn main(){
-
-  std::thread::spawn(move ||{
-    let comando = std::process::Command::new("raspivid").args([
-        "-t","0"
-    ]).arg("-l").args([
-        "-o",
-        "tcp://127.0.0.1:7878",
-    ]).spawn().unwrap();
-    println!("COMANDO: {:?}",comando);
-
-});
+   let comando = std::process::Command::new("raspivid").args([
+       "-t","0"
+   ]).arg("-l").args([
+      "-o",
+      "tcp://127.0.0.1:7878",
+   ]).spawn().unwrap();
 
   let mut stream = TcpStream::connect("127.0.0.1:7878").unwrap();
-  let mut buffer = [0;128];
-  stream.read(&mut buffer).unwrap();
-
-  println!("BUFFER: {:?}",buffer);
+  loop{
+    let mut buffer = [0;128];
+    stream.read(&mut buffer).unwrap();
+    println!("BUFFER: {:?}",buffer);
+  }
   /*loop{
     let mut cliente = ClientBuilder::new("ws://192.168.100.10:3000/ws").unwrap().connect_insecure().unwrap();
     cliente.send_message(&websocket::Message::text("hola")).unwrap();
