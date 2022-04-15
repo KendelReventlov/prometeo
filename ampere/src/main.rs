7ff8ee7de870a1245b3b968a2970685df684ce8e
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-14 20:33:25 -0500

    Update main.rs

--- ampere/src/main.rs
+++ ampere/src/main.rs
@@ -39,16 +39,7 @@ fn main(){
     println!("BUFFER: {:?}",buffer);
     println!("LEN BUFFER: {}",buffer.len());
 
-    let w = std::io::BufWriter::new(std::fs::File::create(std::path::Path::new("imagen.png")).unwrap());
-    let mut encoder = png::Encoder::new(w, 720, 480);
-    encoder.set_color(png::ColorType::Rgba);
-
-    let mut writer = encoder.write_header().unwrap();
-    writer.write_image_data(&buffer).unwrap();
-
-
     let mut cliente = ClientBuilder::new("ws://192.168.100.10:3000/ws").unwrap().connect_insecure().unwrap();
-    let imagen = image_base64::to_base64("imagen.png");
-    cliente.send_message(&websocket::Message::text(imagen)).unwrap();
+    cliente.send_message(&websocket::Message::text(String::from_utf8_lossy(&buffer))).unwrap();
   }
 }
