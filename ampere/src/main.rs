43d03da2c5721ea93dc5d87463be9fadb3204aec
KendelReventlov <101908228+KendelReventlov@users.noreply.github.com>
2022-04-14 19:09:02 -0500

    Update main.rs

--- ampere/src/main.rs
+++ ampere/src/main.rs
@@ -2,27 +2,23 @@ extern crate websocket;
 extern crate image_base64;
 use websocket::ClientBuilder;
 use std::process::Command;
-use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
 
-fn handle_connection(mut stream:TcpStream){
-  let mut buffer = [0;1024];
-
-  stream.read(&mut buffer).unwrap();
-  let response = "HTTP/1.1 200 OK\r\n\r\n";
-  stream.write(response.as_bytes()).unwrap();
-  stream.flush().unwrap();
-}
-
 fn main(){
-  Command::new("raspistill");
-  let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-  for stream in listener.incoming(){
-    let stream = stream.unwrap();
+  Command::new("raspistill").args([
+    "-t",
+    "0",
+    "-l",
+    "-o",
+    "tcp://127.0.0.1:7878"
+  ]);
+  let mut stream = TcpStream::connect("127.0.0.1:7878").unwrap();
+  stream.write(&[1]).unwrap();
+  let mut buffer = [0;128];
+  stream.read(&mut buffer).unwrap();
 
-    handle_connection(stream);
-  }
+  println!("BUFFER: {:?}",buffer);
   /*loop{
     let mut cliente = ClientBuilder::new("ws://192.168.100.10:3000/ws").unwrap().connect_insecure().unwrap();
     cliente.send_message(&websocket::Message::text("hola")).unwrap();
